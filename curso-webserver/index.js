656fb8b5ac18a4fa9d30251223a8ac8871742a86
holatracert <hola@tracertsystem.net>
2025-08-20 23:05:07 -0300

    vamos a crear un servidor web con un reloj que muestra la hora

--- curso-webserver/index.js
+++ curso-webserver/index.js
@@ -1,1 +1,20 @@
-console.log("Hola mundo!");
+
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/hora', (req, res) => {
+    res.send (new Date()
+        .toLocaleTimeString('es-ES', {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
+        }),
+    );
+});
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+    console.log("Hola mundo!");
+});
